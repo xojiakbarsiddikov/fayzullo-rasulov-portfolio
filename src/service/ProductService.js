11855502b61aa4c26d7c64898a62ce7d03fb9214
xojiakbar <103825059+xojiakbarsiddikov@users.noreply.github.com>
2025-01-24 17:32:15 +0500

    first commit

---
+++ src/service/ProductService.js
@@ -0,0 +1,53 @@
+export class ProductService {
+    constructor () {
+        this.contextPath = import.meta.env.BASE_URL;
+    }
+
+    getProductsSmall () {
+        return fetch(this.contextPath + 'demo/data/products-small.json', { headers: { 'Cache-Control': 'no-cache' } })
+            .then(res => res.json())
+            .then(d => d.data);
+    }
+
+    getProductsSchool () {
+        return fetch(this.contextPath + 'demo/data/products-school.json', { headers: { 'Cache-Control': 'no-cache' } })
+            .then(res => res.json())
+            .then(d => d.data);
+    }
+
+    getProductsProgramming () {
+        return fetch(this.contextPath + 'demo/data/products-programming.json', { headers: { 'Cache-Control': 'no-cache' } })
+            .then(res => res.json())
+            .then(d => d.data);
+    }
+
+    getProductsOthers () {
+        return fetch(this.contextPath + 'demo/data/products-others.json', { headers: { 'Cache-Control': 'no-cache' } })
+            .then(res => res.json())
+            .then(d => d.data);
+    }
+
+    getProducts () {
+        return fetch(this.contextPath + 'demo/data/products.json', { headers: { 'Cache-Control': 'no-cache' } })
+            .then(res => res.json())
+            .then(d => d.data);
+    }
+
+    getProductsMixed () {
+        return fetch(this.contextPath + 'demo/data/products-mixed.json', { headers: { 'Cache-Control': 'no-cache' } })
+            .then(res => res.json())
+            .then(d => d.data);
+    }
+
+    getProductsWithOrdersSmall () {
+        return fetch(this.contextPath + 'demo/data/products-orders-small.json', { headers: { 'Cache-Control': 'no-cache' } })
+            .then(res => res.json())
+            .then(d => d.data);
+    }
+
+    getProductsWithOrdersLarge () {
+        return fetch(this.contextPath + 'demo/data/products-orders.json', { headers: { 'Cache-Control': 'no-cache' } })
+            .then(res => res.json())
+            .then(d => d.data);
+    }
+}
---
+++ src/views/pages/Help.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+        <div class="card">
+            <div class="relative overflow-hidden h-20rem bg-primary flex flex-column align-items-center justify-content-center border-round mb-8" style="margin-bottom: 3rem !important;">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="absolute w-full top-0 left-0">
+                    <path
+                        fill="var(--primary-600)"
+                        fillOpacity="1"
+                        d="M0,64L48,90.7C96,117,192,171,288,208C384,245,480,267,576,256C672,245,768,203,864,165.3C960,128,1056,96,1152,74.7C1248,53,1344,43,1392,37.3L1440,32L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+                    ></path>
+                </svg>
+                <div class="font-bold mb-3 text-6xl z-1" data-aos="fade-down"><span class="blue-color_2">College Applicant - </span> RASULOV FAYZULLO</div>
+                <div class="font-bold mb-2 text-2xl z-1" data-aos="fade-right">Age: <span class="blue-color_2">17 YEARS OLD</span></div>
+                <div class="font-bold mb-2 text-2xl z-1" data-aos="fade-left">Major: <span class="blue-color_2">COMPUTER SCIENCE</span></div> 
+                <div class="font-bold mb-2 text-2xl z-1" data-aos="fade-up">Country: <span class="blue-color_2">UZBEKISTAN</span></div> 
+            </div>
+
+            <div class="grid mb-5">
+                <div class="card px-4 py-8 md:px-6 lg:px-8">
+        <div class="flex flex-wrap mb-4" style="align-items: center;">
+            <div class="w-full lg:w-6 pl-0 lg:pr-4" data-aos="fade-right">
+                <img src="/demo/images/my-avatar.jpg" alt="Image" class="w-full border-round" />
+            </div>
+            <div class="w-full lg:w-6 pr-0 lg:pl-4 mt-3 lg:mt-0" data-aos="fade-down">
+                <div class="font-bold text-4xl mb-4 text-900 text-media">Perosnal Statement<span class="blue-color2">(ESSAY)</span></div>
+                <p class="line-height-3 mt-0 mb-3 p-0">
+                    From a young age, I’ve been captivated by the power of technology to create, connect, and inspire. My fascination began with a single moment—stumbling upon a website that felt like a work of art. I was amazed at how something so dynamic could exist online and immediately became curious about the people behind it. That moment planted a seed that grew into a passion for web development and a drive to one day create experiences that could spark the same inspiration in others.
+<br/>
+<br/>
+In high school, I started exploring programming under the guidance of my IT teacher and through my own curiosity. Learning the fundamentals of coding was like unlocking a new language—one that allowed me to bring ideas to life. I vividly remember building my first functional website, an experience that cemented my love for web development. It wasn’t just about writing code; it was about solving problems, being creative, and seeing the tangible impact of my efforts.
+<br/>
+<br/>
+Outside of the classroom, I actively sought opportunities to refine my skills and challenge myself. For over two years, I volunteered as a teacher assistant for my IT teacher, helping to design lessons that made learning engaging and interactive. I also explored freelance projects that introduced me to real-world challenges and deepened my understanding of full-stack development. Volunteering at coding competitions gave me the chance to not only participate but also mentor others, which strengthened my leadership skills and taught me the importance of collaboration in tech.
+<br/>
+<br/>
+One of the highlights of my high school journey was excelling in the school-organized Olympiads, where I proudly took first place. These experiences not only showcased my critical thinking and technical abilities but also reinforced my determination to grow and push my limits in everything I do.
+<br/>
+<br/>
+What excites me most about your university is its unique approach to Computer Science—combining innovation, collaboration, and a commitment to hands-on learning. The opportunity to work on cutting-edge projects, learn from distinguished faculty, and engage with a vibrant community of like-minded individuals is unparalleled. I’m particularly eager to explore courses in web technologies and full-stack development while contributing to research and initiatives that push the boundaries of what’s possible in technology.
+<br/>
+<br/>
+In the future, I aspire to become a skilled developer who creates impactful digital tools and experiences that improve lives and inspire others. I believe this university will provide me with the resources, guidance, and opportunities to achieve that goal. I am excited to embrace the challenges of university life and make a meaningful contribution to its dynamic community. 
+                </p>
+            </div>
+        </div>
+    </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="scss">
+    .blue-color_2 {
+        color: #1b2536;
+    }
+    .blue-color2 {
+        color: #6366f1;
+    }
+    @media screen and (max-width: 610px){
+        .text-6xl {
+            font-size: 2.2rem !important;
+            text-align: center;
+        }
+        .text-media {
+            text-align: center;
+        }
+    }
+</style>
